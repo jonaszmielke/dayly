8513479d479de0237a9a76b9b31116a3f28bb106
Jonasz Mielke <jonasz.mielke@gmail.com>
2026-05-19 00:19:47 +0200

    Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  tseslint.configs.recommended,
+
+  {
+    rules: { curly: "off" },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      curly: "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
